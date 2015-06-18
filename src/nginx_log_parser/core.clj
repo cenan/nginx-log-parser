c2b931a1cc27f6f03699ed46f8212bc2513fc5a9
cenan <me@cenanozen.com>
2015-06-18 22:45:07 +0300

    cleanup

--- src/nginx_log_parser/core.clj
+++ src/nginx_log_parser/core.clj
@@ -3,13 +3,11 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-(defn parse-line
-  [ln]
-  {:status (nth (str/split (nth (str/split ln #"\"") 2) #" ") 1)})
-
 (defn get-status
   [ln]
-  (nth (str/split (nth (str/split ln #"\"") 2) #" ") 1))
+  (nth (str/split (nth (str/split ln #"\"") 2)
+                  #" ")
+        1))
 
 (defn inc-status
   [map key]
@@ -19,8 +17,11 @@
   [sq]
   (loop [result {} sq2 sq]
     (if (empty? sq2)
-    result
-    (recur (merge result (inc-status result (get-status (first sq2)))) (rest sq2)))))
+      result
+      (recur (merge result
+                    (inc-status result
+                                (get-status (first sq2))))
+             (rest sq2)))))
 
 (defn line-count [rdr]
   (count (line-seq rdr)))
@@ -28,4 +29,4 @@
 (defn -main
   [& args]
   (with-open [rdr (io/reader "access.log")]
-   (println (analyze-status (line-seq rdr)))))
+   (println (sort-by #(first %) (analyze-status (line-seq rdr))))))
